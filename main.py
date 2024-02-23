5488fcd152663480933c5b26d3efcbaf5477db06
Guillo659 <guillermoivansalgadosandoval@gmail.com>
2024-02-23 14:31:15 -0500

    Day0

---
+++ main.py
@@ -0,0 +1,10 @@
+class perro():
+    def __init__(self):
+        print("Perro creado")
+
+    def write(self, raza, nombre, edad):
+        print(raza, nombre, edad)
+
+perro = perro()
+perro.write("ascii", "kwerty", 8)
+# Oscar    3008058576

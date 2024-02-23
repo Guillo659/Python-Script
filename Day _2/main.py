5aa1c3172dc1bb2e4bbe7f0d3b6cbb90fe1f30e1
Guillo659 <guillermoivansalgadosandoval@gmail.com>
2024-02-23 15:12:08 -0500

    Day 2, level 2

--- Day _2/main.py
+++ Day _2/main.py
@@ -1,14 +1,24 @@
 # Day 2: 30 Days of python programming
 
 name = "Guillermo"
+print(type(name))
 surname = "Salgado"
+print(type(surname))
 completName = name + surname
+print(type(completName))
 country = "Colombia"
+print(type(country))
 city = "Monteria"
+print(type(city))
 age = 18
+print(type(age))
 year = 2024
+print(type(year))
 is_married = 24.7
+print(type(is_married))
 is_true = True
+print(type(is_true))
 is_light_on = False
+print(type(is_light_on))
 
 a, b, c = "a", "b", "c"

11fc9f83980b5b66a9e812b6f2bb92d3dbcaa7d4
Guillo659 <guillermoivansalgadosandoval@gmail.com>
2024-02-24 10:03:58 -0500

    Day 4

---
+++ Day_4/str.py
@@ -0,0 +1,31 @@
+# Punto 1
+'''
+st1 = "Trety"
+st2 = "Days"
+st3 = "Of"
+st4 = "Python"
+print(f"{st1} {st2} {st3} {st4}")
+
+# Punto 2
+st1 = "Coding"
+st2 = "For"
+st3 = "All"
+print(f"{st1} {st2} {st3}")
+
+# Punto 3
+st = "Coding For All"
+
+# Punto 4
+print(st)
+
+# Punto 5
+print(len(st))
+
+# Punto 6
+print(st.upper())
+'''
+# Punto 7
+st = "coding for all"
+print(st.capitalize())
+print(st.title())
+print(st.swapcase())
